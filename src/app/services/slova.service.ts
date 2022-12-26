784fbd730e03a86780653b4f7706c0c6797b0ed8
Vladimir Cvetkovic <VladimirCvetkovic@users.noreply.github.com>
2022-12-26 19:35:14 +0000

    fixed

--- src/app/services/slova.service.ts
+++ src/app/services/slova.service.ts
@@ -7,17 +7,17 @@ import { SLOVA } from '../shared/mock-slova';
 export class SlovaService {
 
   getSlovaForTable():any{   
-    let returnValue = [];
+    let tableData = [];
     var maxRow = Math.max(...SLOVA.map(o=>o.row)); 
     for (let index = 0; index < maxRow; index++) {
       const filterRow = SLOVA.filter(item => item.row == index + 1);
       let rowObj = {};
       for (const item of filterRow) {
-        if (item.visible) rowObj = { ...rowObj, ["column" + item.column]: `${item.text.toLocaleUpperCase()}, ${item.text} (${item.pronunciation})` }
+        if (item.visible) rowObj = { ...rowObj, ["column" + item.column]: `${item.text.toUpperCase()}, ${item.text} (${item.pronunciation})` }
       }
-      returnValue.push(rowObj);
+      tableData.push(rowObj);
     }
-    return returnValue;
+    return tableData;
   }
 
   spell(word: string): Array<string>{
